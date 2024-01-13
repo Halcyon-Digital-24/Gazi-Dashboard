2c4a2bc7304b43c57eb0ee44728ff7bb15bb2647
zulkar29 <zulkar29@gmail.com>
2024-01-13 13:38:35 +0600

    react query support

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import BaseTemplate from './base-template';
 import ScrollToTop from './components/scroll';
 import { ToastContainer } from 'react-toastify';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 function App() {
+
+  const queryClient = new QueryClient()
+
+
   return (
-    <>
-      <ScrollToTop />
+    <QueryClientProvider client={queryClient}>
+    <ScrollToTop />
       <BaseTemplate />
       <ToastContainer />
-    </>
+    </QueryClientProvider>
   );
 }
 
