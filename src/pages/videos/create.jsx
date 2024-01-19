2630f9ad6750e3dcae05c35361b6d4544313b03b
zulkar29 <zulkar29@gmail.com>
2024-01-19 20:43:31 +0600

    refactor video section

---
+++ src/pages/videos/create.jsx
@@ -0,0 +1,81 @@
+import { useState, ChangeEvent, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import CardBody from '../../components/card-body';
+import Display from '../../components/display';
+import Input from '../../components/forms/text-input';
+import { Button } from '../../components/button';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import axios from "../../lib";
+
+
+const CreateVideo = () => {
+  const navigate = useNavigate();
+
+
+
+  const { register, handleSubmit, formState: { errors }} = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post('/videos', data);
+      navigate('/videos');
+      toast.success(response.data.message);
+    } catch (error) {
+      toast.error('Error saving data');
+      console.error(error);
+    }
+  };
+
+  
+ 
+  return (
+    <div>
+      <CardBody to="/videos" text="back" header="Create Video" />
+
+      <Display>
+        <form onSubmit={handleSubmit(onSubmit)}>          
+<div className="text">
+            <label htmlFor="title">Video Title *</label>
+            <input
+              type="text"
+              {...register("title", {
+                required: "Location is required",
+                pattern: {
+                  value: /^[a-zA-Z ]+$/,
+                  message: "Enter a valid title"
+                }
+              })}
+            />
+            {errors.title && (
+              <p className="validation__error">{errors.title.message}</p>
+            )}
+          </div>
+
+
+          <div className="text">
+            <label htmlFor="title">Video Embed URL *</label>
+            <input
+              type="text"
+              placeholder="Enter video embed code"
+              {...register("url", {
+                required: "url is required",
+                pattern: {
+                  value: /^[a-zA-Z ]+$/,
+                  message: "Enter a valid url"
+                }
+              })}
+            />
+            {errors.url && (
+              <p className="validation__error">{errors.url.message}</p>
+            )}
+          </div>
+
+          <Button type="submit">Save</Button>
+        </form>
+      </Display>
+    </div>
+  );
+};
+
+export default CreateVideo;
