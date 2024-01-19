4f4528afaaf4f30ab3e2c641e0633a157df76a15
zulkar29 <zulkar29@gmail.com>
2024-01-19 22:26:33 +0600

    refactor update faq

--- src/pages/faqs/update.jsx
+++ src/pages/faqs/update.jsx
@@ -10,22 +10,44 @@ import axios from "../../lib";
 const UpdateFaq= () => {
   const navigate = useNavigate();
   const { slug } = useParams();
+  const { register, handleSubmit, setValue, formState: { errors }} = useForm();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/faqs/${slug}`);
+        setValue("question", res.data.data.question);
+        setValue("answer", res.data.data.answer);
+      } catch (error) {
+        console.log("Video fetch error" + error);
+      }
+    };
+    fetchData();
+  }, [slug]);
 
   return (
     <div>
       <CardBody header="Update Faq" to="/faqs" text="back" />
       <Display>
         <form onSubmit={handleSubmit}>
-          <Input
-            name="question"
-            onChange={(e) => setQuestion(e.target.value)}
-            htmlFor="title"
-            label="Question *"
-            placeholder="Question here..."
-            value={question}
-            required
-          />
+
+        <div className="text">
+            <label htmlFor="Question">Question*</label>
+            <input
+              type="text"
+              placeholder="Question here..."
+              {...register("question", {
+                required: "question is required",
+                pattern: {
+                  value: /\S/,
+                  message: "Enter a valid question"
+                }
+              })}
+            />
+            {errors.question && (
+              <p className="validation__error">{errors.question.message}</p>
+            )}
+          </div>
 
           <TextArea
             name="answer"
