127055b9cc6ffb97b0e88a7e5912c45e46062fa6
Halcyon-Digital <zunayed.ali417@gmail.com>
2024-01-28 22:07:26 +0600

    add validation

--- src/redux/comments/commentService.ts
+++ src/redux/comments/commentService.ts
@@ -1,17 +1,17 @@
-import { ICommentResponse } from '../../interfaces/comment';
-import axios from '../../lib';
+import { ICommentResponse } from "../../interfaces/comment";
+import axios from "../../lib";
 
 const getComment = async (filter: {
   [key: string]: string | number;
 }): Promise<ICommentResponse> => {
-  let url = `/comments`;
+  let url = `/blog/comments`;
   if (filter && Object.keys(filter).length > 0) {
     const queryString = Object.entries(filter)
       .map(
         ([key, value]) =>
           `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
       )
-      .join('&');
+      .join("&");
 
     // Add query string to the URL
     url += `?${queryString}`;
