f9a02c712b07325ed712fc83e82495c33ae46d85
Halcyon-Digital <zunayed.ali417@gmail.com>
2024-01-29 19:01:34 +0600

    profile popup

--- src/components/popup/profile.tsx
+++ src/components/popup/profile.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef } from "react";
 import { CgProfile } from "react-icons/cg";
 import { IoIosLogOut } from "react-icons/io";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { CiSettings } from "react-icons/ci";
 import "./profile.scss";
 
 interface PopupProps {
@@ -13,8 +14,7 @@ const ProfilePopup: React.FC<PopupProps> = ({ closePopup }) => {
   const popupRef = useRef<HTMLDivElement>(null);
 
   const handleLogOut = () => {
-    localStorage.removeItem('user');
-    
+    localStorage.removeItem("user");
   };
 
   const handleProfile = () => {
@@ -39,16 +39,24 @@ const ProfilePopup: React.FC<PopupProps> = ({ closePopup }) => {
 
   return (
     <div ref={popupRef} className="profile-popup">
-      <ul>
-        <li className="item" onClick={handleProfile}>
-          <CgProfile className="icon" />
-          <p>Profile</p>
-        </li>
-        <li className="item" onClick={handleLogOut}>
-          <IoIosLogOut className="icon" />
-          <p>Logout</p>
-        </li>
-      </ul>
+      <div className="profile-area">
+        <ul>
+          <li className="item" onClick={handleProfile}>
+            <CgProfile className="icon" />
+            <p>Profile</p>
+          </li>
+          <Link to={"/setup/home-page"}>
+            <li className="item" onClick={handleLogOut}>
+              <CiSettings className="icon" />
+              <p>Settings</p>
+            </li>
+          </Link>
+          <li className="item" onClick={handleLogOut}>
+            <IoIosLogOut className="icon" />
+            <p>Logout</p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
