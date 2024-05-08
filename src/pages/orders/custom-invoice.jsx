2e1b5217ea753bd2a7d998fa47b0cc50d91300c5
G. M. FARHAD HOSSAIN <mdhabiburrb@gmail.com>
2024-05-08 16:31:01 +0600

    custom invoice

--- src/pages/orders/custom-invoice.jsx
+++ src/pages/orders/custom-invoice.jsx
@@ -5,73 +5,29 @@ import Display from "../../components/display";
 import "./custom-order.scss";
 import './custom-invoice.scss'
 import { useEffect, useRef, useState } from "react";
-import { getFrontendProducts, reset } from "../../redux/products/product-slice";
-import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import {
-  addToCart,
-  clearCart,
-  decrementQuantity,
-  incrementQuantity,
-  removeFromCart,
-} from "../../redux/cart/cartSlice";
+import { useAppSelector } from "../../redux/hooks";
 import axios from "../../lib";
 import { API_URL } from "../../constants";
 import { toast } from "react-toastify";
-// import { FaCheck } from "react-icons/fa";
-// import { FiPlus } from "react-icons/fi";
-// import { LuMinus } from "react-icons/lu";
-// import { RxCross2 } from "react-icons/rx";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import DeleteButton from "../../components/button/delete";
+import { FaTimes } from "react-icons/fa";
+
 
 const CustomInvoice = () => {
-  // const { products } = useAppSelector((state) => state.product);
   const { cart: cartItems } = useAppSelector((state) => state.cart);
   const navigate = useNavigate();
-  const dispatch = useAppDispatch();
   const [discount, setDiscount] = useState(0);
   const [shipping, setShipping] = useState(0);
   const [isFocus, setIsFocus] = useState(false);
-  // const [search, setSearch] = useState("");
-  const [productTitle, setProductTitle] = useState("")
-  const [productArray, setProductArray] = useState([])
+  const [productArray, setProductArray] = useState([{
+    product_name: '',
+    product_attribute: '',
+    quantity: '',
+    regular_price: ''
+  }])
   const productAreaRef = useRef(null);
-  const [selectedAttributes, setSelectedAttributes] = useState([]);
   const [advancedPayment, setAdvancedPayment] = useState(0);
-
-  // const handleAttributeClick = (attribute) => {
-  //   const isExists = selectedAttributes.filter(
-  //     (attr) =>
-  //       attr.attribute_id === attribute.id ||
-  //       attr.attribute_key === attribute.attribute_key ||
-  //       attr.attribute_name === attribute.attribute_value
-  //   );
-  //   if (isExists.length < 1) {
-  //     setSelectedAttributes((prev) => [
-  //       ...prev,
-  //       {
-  //         attribute_id: attribute.id,
-  //         attribute_name: attribute.attribute_value,
-  //         attribute_quantity: 1,
-  //         attribute_key: attribute.attribute_key,
-  //       },
-  //     ]);
-  //   } else {
-  //     const sameType = selectedAttributes.filter(
-  //       (attr) => attr.attribute_key !== attribute.attribute_key
-  //     );
-  //     setSelectedAttributes([
-  //       ...sameType,
-  //       {
-  //         attribute_id: attribute.id,
-  //         attribute_name: attribute.attribute_value,
-  //         attribute_quantity: 1,
-  //         attribute_key: attribute.attribute_key,
-  //       },
-  //     ]);
-  //   }
-  // };
 
   const {
     register,
@@ -97,57 +53,42 @@ const CustomInvoice = () => {
     0
   );
 
-  // const orderItem = cartItems.map((item) => ({
-  //   product_id: item.product_id,
-  //   quantity: item.quantity,
-  //   attribute: item?.attribute,
-  // }));
-  /*  const orderData = {
-    name,
-    email,
-    mobile,
-    address,
-    city,
-    thana,
-    order_form: "custom",
-    final_price: final_price + shipping - discount,
-    delivery_fee: 0,
-    payment_method: "custom",
-    order_status: "pending",
-    delivery_method: "custom",
-    orderItem,
-  }; */
 
   const onSubmit = async (data) => {
     if (productArray.length <= 0) {
       toast.error("Please select product");
     } else {
       try {
-        const response = await axios.post(`${API_URL}/orders`, {
-          ...data,
-          orderItem: productArray,
+        console.log('data', {
+          ...data, orderItem: productArray,
           delivery_fee: shipping,
           advance_payment: advancedPayment,
           custom_discount: discount,
-        });
+        })
+
+        let temp = [...productArray];
+        for (let i = 0; i < temp.length; i++) {
+          temp[i].product_id = i + 1;
+        }
+
+        const response = await axios.post(`${API_URL}/invoices`, {
+          ...data,
+          orderItem: temp,
+          delivery_fee: shipping,
+          advance_payment: advancedPayment,
+          custom_discount: discount
+
+        })
+        console.log('resp', response)
         toast.success(response.data.message);
-        dispatch(clearCart());
-        navigate("/orders");
-        // window.location.reload();
+        // navigate("/orders");
+        window.location.reload();
       } catch (error) {
         toast.error(error?.response?.data?.message);
         console.log(error);
       }
     }
   };
-
-  // useEffect(() => {
-  //   dispatch(getFrontendProducts({ search: search, page: 1, limit: 50 }));
-
-  //   return () => {
-  //     dispatch(reset());
-  //   };
-  // }, [search, dispatch]);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -167,30 +108,31 @@ const CustomInvoice = () => {
   }, [isFocus]);
 
 
-  const handleAddProduct = () => {
-    if (!productTitle || productTitle === '') return;
-
-    const titleField = document.getElementById('product_title')
-
-    let tempProducts = [...productArray];
-    tempProducts.push(productTitle)
-    console.log("productArray11", tempProducts, productTitle)
-
-    setProductArray(tempProducts);
-    setProductTitle('')
-    if (titleField && titleField.value) titleField.value = ""
+  const addNewLine = () => {
+    let temp = [...productArray]
+    temp.push({
+      product_name: '',
+      product_attribute: '',
+      quantity: '',
+      regular_price: ''
+    })
+    setProductArray(temp)
+  }
+  const onChangeProduct = (value, index, key) => {
+    let temp = [...productArray]
+    if (temp[index]) {
+      temp[index][key] = value
+      setProductArray(temp)
+    }
 
   }
 
-
-  const deleteProductTitle = (index) => {
+  const handelDelete = (index) => {
     let tempProducts = [...productArray];
     tempProducts = tempProducts.filter((_, i) => index != i)
-
     setProductArray(tempProducts)
   }
 
-  // console.log("productArray",productArray)
 
   return (
     <div>
@@ -292,6 +234,9 @@ const CustomInvoice = () => {
                     <p className="validation__error">{errors.city?.message}</p>
                   )}
                 </div>
+
+              </div>
+              <div className="col-md-4 custom-item">
                 <Input
                   type="number"
                   htmlFor="shipping"
@@ -299,8 +244,6 @@ const CustomInvoice = () => {
                   label="Shipping Price"
                   onChange={(e) => setShipping(Number(e.target.value))}
                 />
-              </div>
-              <div className="col-md-4 custom-item">
                 <Input
                   type="number"
                   htmlFor="discount"
@@ -387,39 +330,9 @@ const CustomInvoice = () => {
                     </p>
                   )}
                 </>
-                {/* <Input htmlFor="variant" placeholder="variant" /> */}
-                <div className="product-area" ref={productAreaRef}>
-                  <Input
-                    htmlFor="product_title"
-                    placeholder="Product Title"
-                    label="Product Title"
-                    onChange={(e) => setProductTitle(e.target.value)}
-                    autocomplete="off"
-                  >
-                    <div className="children">
-                      <button className="btn btn-danger" type='button' onClick={() => handleAddProduct()}>Add</button>
-
-                    </div>
-                  </Input>
-                  {
-                    productArray?.length ?
-                      <div className="product-list-sec">
-                        {
-                          productArray?.map((item, index) => {
-                            return (
-                              <div className="single-title" key={index}>
-                                <span>{index + 1}{'. '}{item}</span>
-                                {/* <button className="btn btn-danger">X</button> */}
-                                <DeleteButton type='button' onClick={() => deleteProductTitle(index)} />
-                              </div>
-                            )
-                          })
-                        }
-                      </div> : null
-                  }
-
-                </div>
               </div>
+              {/* <Input htmlFor="variant" placeholder="variant" /> */}
+
               <div className="col-md-3 custom-item">
                 <div className="summery">
                   <div className="row">
@@ -436,6 +349,81 @@ const CustomInvoice = () => {
                       {final_price + shipping - discount - advancedPayment}
                     </div>
                   </div>
+                </div>
+              </div>
+              <div className="col-md-9 custom-item">
+
+                <div className="product-area" ref={productAreaRef}>
+
+                  {
+                    productArray?.map((item, index) => {
+                      return (
+                        <div className="product-form w-100" key={index}>
+                          <div style={{ width: "50%", marginRight: '2%' }}>
+                            <Input
+                              required
+                              htmlFor="product_name"
+                              placeholder="Product Title"
+                              // label="Product Title"
+                              value={item?.product_name}
+                              onChange={(e) => onChangeProduct(e.target.value, index, 'product_name')}
+                              autocomplete="off"
+                            />
+                          </div>
+
+                          <div style={{ width: "24%", marginRight: '2%' }}>
+                            <Input
+                              htmlFor="product_attribute"
+                              placeholder="Product Attributes"
+                              // label="Product Attributes"
+                              value={item?.product_attribute}
+
+                              onChange={(e) => onChangeProduct(e.target.value, index, "product_attribute")}
+                              autocomplete="off"
+                            />
+                          </div>
+                          <div style={{ width: "10%", marginRight: '2%' }}>
+                            <Input
+                              required
+
+                              htmlFor="quantity"
+                              placeholder="Product Quantity"
+                              value={item?.quantity}
+                              // label="Product Quantity"
+                              onChange={(e) => onChangeProduct(e.target.value, index, "quantity")}
+                              autocomplete="off"
+                            />
+                          </div>
+                          <div style={{ width: "10%" }}>
+                            <Input
+                              required
+
+                              htmlFor="regular_price"
+                              placeholder="Product Price"
+                              // label="Product Price"
+                              value={item?.regular_price}
+
+                              onChange={(e) => onChangeProduct(e.target.value, index, "regular_price")}
+                              autocomplete="off"
+                            />
+                          </div>
+                          {
+                            productArray?.length > 1 ?
+                              <div className="delete-button">
+                                <button type="button" onClick={() => handelDelete(index)}><FaTimes />  </button>
+                              </div> : null
+                          }
+                        </div>
+                      )
+                    })
+                  }
+
+                  <div className="create-button">
+                    <Button type="button" onClick={addNewLine}>Add Line</Button>
+
+                  </div>
+
+
                 </div>
               </div>
             </div>
